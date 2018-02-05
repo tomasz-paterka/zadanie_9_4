0da667a9a3b0e170101182ebfaf220e0610a90d8
Tomasz Paterka <tomekpaterka@gmail.com>
2018-02-05 17:06:19 +0100

    Add function drawTree

--- js/script.js
+++ js/script.js
@@ -1,7 +1,25 @@
-for (var i = 0; i < 10; i++) {
-    var star = '';
-    for (var j = 0; j <= i; j++) {
-        star += '*';
-    }
-    console.log(star);
+function drawTree(k) {
+	for (var i = 0; i < k; i++) {
+		var star = '';
+		for (var j = 0; j <= i; j++) {
+			star += '*';
+		}
+		console.log(star);
+	}
 }
+drawTree(6);
+drawTree(7);
+
+
+
+/*var txt = "";
+for(i=1;i<=10;i++){
+  txt = "";
+  for(j=i;j<10;j++){
+    txt += " ";
+  }
+  for(k=0;k<(i*2)-1;k++){
+    txt += "*";
+  }
+  console.log(txt);
+}*/
